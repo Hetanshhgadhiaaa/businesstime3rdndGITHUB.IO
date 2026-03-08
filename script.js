55c9f1106490e65d22105a01f922b618f4a7e7a1
Hetanshhgadhiaaa <hetanshhgadhiya@gmail.com>
2026-03-08 17:08:37 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,25 +1,28 @@
-export default {
-  async fetch(request) {
+async function sendMessage() {
 
-    if (request.method === "POST") {
+  const inputBox = document.getElementById("chatInput");
+  const chatBox = document.getElementById("chatBox");
 
-      const data = await request.json();
-      const message = data.message;
+  const userMessage = inputBox.value;
 
-      return new Response(
-        JSON.stringify({
-          reply: "You said: " + message
-        }),
-        {
-          headers: {
-            "Content-Type": "application/json",
-            "Access-Control-Allow-Origin": "*"
-          }
-        }
-      );
+  if (!userMessage) return;
 
-    }
+  chatBox.innerHTML += "<p><b>User:</b> " + userMessage + "</p>";
 
-    return new Response("Study AI Worker Running");
-  }
-};
+  inputBox.value = "";
+
+  const response = await fetch("https://businesstime3rdndgithub-io.hetanshhgadhiaaa.workers.dev", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+      message: userMessage
+    })
+  });
+
+  const data = await response.json();
+
+  chatBox.innerHTML += "<p><b>AI:</b> " + data.reply + "</p>";
+
+}
