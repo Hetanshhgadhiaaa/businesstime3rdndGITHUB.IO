78e5d61eb2ed6ab6457ee908289a2a75f204b8e1
Hetanshhgadhiaaa <hetanshhgadhiya@gmail.com>
2026-03-08 17:13:15 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,28 +1,9 @@
-async function sendMessage() {
-
-  const inputBox = document.getElementById("chatInput");
-  const chatBox = document.getElementById("chatBox");
-
-  const userMessage = inputBox.value;
-
-  if (!userMessage) return;
-
-  chatBox.innerHTML += "<p><b>User:</b> " + userMessage + "</p>";
-
-  inputBox.value = "";
-
-  const response = await fetch("https://businesstime3rdndgithub-io.hetanshhgadhiaaa.workers.dev", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json"
-    },
-    body: JSON.stringify({
-      message: userMessage
-    })
-  });
-
-  const data = await response.json();
-
-  chatBox.innerHTML += "<p><b>AI:</b> " + data.reply + "</p>";
-
-}
+const response = await fetch("https://businesstime3rdndgithub-io.hetanshhgadhiaaa.workers.dev", {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json"
+  },
+  body: JSON.stringify({
+    message: userMessage
+  })
+});
